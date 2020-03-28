5fd35aacd5e0c7c296070b55ff7a8bbe2fec920f
saeboram <61257531+saeboram@users.noreply.github.com>
2020-03-28 22:10:29 +0900

    Add files via upload

---
+++ 3주차_숙제.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+head1 = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=head1)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+musics = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+# musics (tr들) 의 반복문을 돌리기
+
+rank = 1
+for music in musics:
+    # music 안에 a 가 있으면,
+    a_tag = music.select_one('tr.list > td.info')
+    if a_tag is not None :
+        title = music.select_one('td.info > a.title').text
+        artist = music.select_one('td.info > a.artist').text
+        print (rank, title.strip(), artist)
+        rank += 1
